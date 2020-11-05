28897dd5f3b761455d3dca42ad0e562539944b54
vqf <quesadavictor@uniovi.es>
2020-11-05 18:33:16 +0100

    2 files modified, 1 added

--- experiment.js
+++ experiment.js
@@ -1,15 +1,3 @@
-function loadDependencies(scs) {
-  scs.forEach(sc => {
-    let sid = "ld" + sc.replace(/\./g, "_");
-    let s = document.getElementById(sid);
-    if (s === null) {
-      s = document.createElement("script");
-      s.type = "text/javascript";
-      s.id = sid;
-      s.src = sc;
-      document.head.appendChild(s);
-    }
-  });
-}
+
 
 loadDependencies(["pipette.js"]);
---
+++ master.js
@@ -0,0 +1,63 @@
+function _uid() {
+  let l = 10;
+  let result = "";
+  let letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p"
+  ];
+  for (let i = 0; i < l; i++) {
+    let tl = 0;
+    for (let j = 0; j < 4; j++) {
+      tl = tl << 1;
+      let r = Math.random();
+      if (r > 0.5) {
+        tl++;
+      }
+    }
+    result += letters[tl];
+  }
+  return result;
+}
+
+function loadTag(dsc) {
+  if (typeof dsc === "object") {
+    if (dsc.isArray() === true) {
+      dsc.forEach(el => {
+        loadTag(el);
+      });
+    } else {
+      let sid = uid();
+      if (dsc.)
+    }
+  }
+}
+
+function loadDependencies(scs) {
+  scs.forEach(sc => {
+    let sid = "ld" + sc.replace(/\./g, "_");
+    let s = document.getElementById(sid);
+    if (s === null) {
+      s = document.createElement("script");
+      s.type = "text/javascript";
+      s.id = sid;
+      s.src = sc;
+      document.head.appendChild(s);
+    }
+  });
+}
+
+loadTag({ name: "yo" });
