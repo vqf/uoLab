50a90eae4f08988d1b2fbaa79134d384f38ed328
vqf <quesadavictor@uniovi.es>
2020-11-15 19:27:08 +0100

    1 file modified

--- experiment.js
+++ experiment.js
@@ -2,18 +2,4 @@ let scr = document.getElementById("screen");
 let pipette1 = new pipette(scr);
 pipette1.inject(100, 100);
 
-pipette1.scale(2, 2);
-pipette1.move(100, -100, "5s");
-
-pipette1.rotate(45);
-
-let pipette2 = new pipette(scr);
-pipette2.inject(150, 100);
-pipette2.scale(2, 2);
-pipette2.rotate(-45);
-
-let yt1 = new yellowTip(scr);
-yt1.scale(2, 2);
-yt1.inject(150, 100);
-
 if (DEBUG > 0) console.log("Experiment loaded");
